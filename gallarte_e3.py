c3eb70d22d7bf5f19805a6d41841e941b5d973a5
Abraham Gallarte <git-email>
2018-10-26 16:22:02 +0800

    try lang

--- gallarte_e3.py
+++ gallarte_e3.py
@@ -1,13 +1,11 @@
 
-message = input ("Enter a comma separated list of numbers:")
+data = input ("Enter a comma separated list of numbers:")
+numbers= data.split(",")
 
-x=1 
-total = 0
-while x < 10:
-   if x & 1 :
-        
-        print(x**2, end= " ")
-        total += x
-   x += 1
 
-   print ("total=", total)
+total = 0.0
+for x in range(len(numbers)):
+    G = float(numbers[i])
+    total== G * G 
+   
+    print ("sum of squares=", total)
